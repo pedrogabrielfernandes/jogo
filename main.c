bf630487004596374dd2e09ef8c8845702e436de
pedrogabrielfernandes <alvesfernandespedrogabriel@gmail.com>
2026-04-13 18:46:59 -0300

    fiz o bonequinho andar

--- main.c
+++ main.c
@@ -34,6 +34,10 @@ int main() {
         printf("falha no carramento da imagem");
         return 1;
     }
+    if (!al_install_keyboard()){
+        printf("falha na inicialização do teclado.");
+    }
+    
 
     //criando o evento de fila e fontes>
     ALLEGRO_EVENT_QUEUE *queue = al_create_event_queue();
@@ -53,6 +57,7 @@ int main() {
     //definindo o fps
     ALLEGRO_TIMER *timer = al_create_timer(1.0/30.0);
     al_register_event_source(queue, al_get_timer_event_source(timer));
+    al_register_event_source(queue, al_get_keyboard_event_source());
     al_start_timer(timer);
     
 
@@ -65,6 +70,13 @@ int main() {
 
     //definindo os sprites do personagem
     ALLEGRO_BITMAP *idle = al_load_bitmap("assets/sprites/IDLE.png");
+    ALLEGRO_BITMAP *run = al_load_bitmap("assets/sprites/RUN.png");
+    ALLEGRO_BITMAP *jump = al_load_bitmap("assets/sprites/JUMP.png");
+    if (!idle || !run || !jump){
+        printf("falha ao carregar os sprites");
+        return 1;
+    }
+    
 
     //um evento
     ALLEGRO_EVENT evento;
@@ -77,29 +89,63 @@ int main() {
     int pos_y = 560;
     int current_frame_y = 0;
 
+    ALLEGRO_KEYBOARD_STATE state;
+
     //o laco de repeticao que vai fazer o jogo rodar ate fechar
     while (rodando){
             al_wait_for_event(queue, &evento);
             if (evento.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
                 rodando = 0;
             }
+
+  
             if (evento.type == ALLEGRO_EVENT_TIMER){
                 frame += 0.3f;
 
-                if (frame > 7){
-                    frame -= 7;
-                }
-                
-
                 al_clear_to_color(al_map_rgb(255, 255, 255));    
                 al_draw_bitmap(bg, 0, 0, 0);
+                al_get_keyboard_state(&state);
+
+                if (al_key_down(&state, ALLEGRO_KEY_RIGHT)){
+                    if (frame > 8){
+                    frame -= 8;
+                    }
+                    pos_x += 5;
+                    al_draw_bitmap_region(run, 96 * (int) frame, current_frame_y, 96, 84, pos_x, pos_y, 0);
+                }
+
+                 else if (al_key_down(&state, ALLEGRO_KEY_LEFT)){
+                    if (frame > 8){
+                    frame -= 8;
+                    }
+                    pos_x -= 5;
+                    al_draw_bitmap_region(run, 96 * (int) frame, current_frame_y, 96, 84, pos_x, pos_y, 0);
+                }
+
+                else if (al_key_down(&state, ALLEGRO_KEY_SPACE)){
+                    if (frame > 5){
+                    frame -= 5;
+                    }
+                    pos_y -= 40;
+                    al_draw_bitmap_region(jump, 96 * (int) frame, current_frame_y, 96, 84, pos_x, pos_y, 0);
+                    pos_y = 560;
+                }
+
+                else{
+                    if (frame > 7){
+                    frame -= 7;
+                    }
+                    al_draw_bitmap_region(idle, 96 * (int) frame, current_frame_y, 96, 84, pos_x, pos_y, 0);
+                }
+                
                 //al_draw_bitmap(idle, 0, 560, 0);
-                al_draw_bitmap_region(idle, 96 * (int) frame, current_frame_y, 96, 84, pos_x, pos_y, 0);
                 al_draw_text(fonte, al_map_rgb(0, 0, 0), 100, 100, 0, "Entardecer");
                 al_flip_display();
             }
     }
     al_destroy_bitmap(idle);
+    al_destroy_bitmap(run);
+    al_destroy_bitmap(jump);
     al_destroy_bitmap(bg);
     al_destroy_display(display);
     al_destroy_font(fonte);
@@ -109,35 +155,4 @@ int main() {
     
 }
 
-/*
-    se mexer no github dê no terminal zsh: git pull origin main --rebase
-    isso vai sincronizar os arquivos do vscode com o github.
-
-    (Linux) -> sudo "..." github-cli
-    (Linux) -> gh auth login - faz o login
-    (Linux) -> git clone https://github.com/pedrogabrielfernandes/jogo
-    (Linux) -> cd jogo/ - na pasta "jogo" 
-    
-    *Comando Universal ->
-    gcc main.c -o teste -lallegro -lallegro_main -lallegro_ttf -lallegro_font -lallegro_image -> compilar o codigo
-    ./teste -> executa o codigo
-
-    após cada alteração que fizerem dê ctrl+s atualizem o repositório do github, exemplo: 
-
-    git add .
-    git commit -m "o que você alterou"
-    git push
-    
-    logo após atualize seu repositorio e atualize o seu gitclone
-
-    ** SEU TERMINAL -> PESSOAL (atualizar o seu gitclone)
-    (dentro da pasta jogo)
-    git stash  -> salva suas alterações
-    git pull --rebase origin main  -> puxa as alterações do main original
-    git stash pop  -> implementa o main original e volta com suas alterações pessoais
-
-    *Atualizou ->
-    gcc main.c -o teste -lallegro -lallegro_main -lallegro_ttf -lallegro_font -lallegro_image -> compilar o codigo atualizado
-    ./teste -> executar o codigo atualizado
-    
-*/
+//se tem duvidas, leia as instuções na pasta do jogo.
